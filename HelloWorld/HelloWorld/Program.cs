3ed2b5ba90ee0431368271f010b6dab0a9984ffb
Pedde136 <pedramkhanzadenvakilkandi@elev.ga.ntig.se>
2020-08-31 13:18:41 +0200

    hej2

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,25 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello RoadToGlobal");
+
+            Console.WriteLine("Enter your age:");
+           
+            var age = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Your age is: " + age);
+
+            Console.WriteLine("Enter name:");
+
+            var Name = Console.ReadLine();
+
+            Console.WriteLine("Your name is: " + Name);
+
+            Console.WriteLine("Are you alive " + Name + "?");
+
+            var alive = Console.ReadLine();
+
+            Console.WriteLine("Alive: " + alive);
+
         }
     }
 }
