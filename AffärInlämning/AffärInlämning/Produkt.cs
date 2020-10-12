a425805e7163ecef944f7113fcc8b5c559a2e86c
Pedde136 <pedramkhanzadenvakilkandi@elev.ga.ntig.se>
2020-10-12 13:54:41 +0200

    hej

---
+++ AffärInlämning/AffärInlämning/Produkt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AffärInlämning
+{
+    class Produkt
+    {
+        private string _productName;
+        public string Name
+        {
+            get { return _productName; }
+            set { _productName = value; }
+        }
+       
+        private int _price;
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+    }
+}
---
+++ AffärInlämning/AffärInlämning/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AffärInlämning
+{
+    class Program
+    {
+        private static List<Produkt> kundvagn = new List<Produkt>();
+        static void Main(string[] args)
+        {
+ 
+            MenuText();
+
+            Produkt pölse = new Produkt();
+            pölse.Name = "Pölse";
+            pölse.Price = 15;
+
+            int caseSwitch = Convert.ToInt32(Console.ReadLine());
+
+            switch (caseSwitch)
+            {
+                case 1:
+
+                    Console.WriteLine("Whats your name?");
+
+                    Kund kund = new Kund();
+                    
+                    kund.Name = Console.ReadLine();
+                    
+                    Console.WriteLine("Whats you age?");
+                    
+                    kund.Age = Convert.ToInt32(Console.ReadLine());
+                    
+                    Console.WriteLine("Hey, my name is " + kund.Name + " and I'm " + kund.Age + " years old. Let's shop. \n");
+
+                    kundvagnAdd(pölse);
+                    
+                    viewKundvagn();
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("Exit");
+                    Environment.Exit(1);
+                    break;
+            }
+
+        }
+        public static void MenuText()
+        {
+            Console.WriteLine("Hello and welcome to my shop. Please select an alternative: \n");
+
+            Console.WriteLine("Press (1) to Start Shopping");
+
+            Console.WriteLine("Press (2) to Exit");
+        }
+
+        public static void kundvagnAdd(Produkt pölse)
+        {
+            Console.WriteLine("Press (1) to add ¨Pölse¨ to cart. \n");
+            if (Console.ReadLine() == "1")
+            {
+                kundvagn.Add(pölse);
+                Console.WriteLine("'Pölse' has been added to cart! \n");
+            }
+        }
+
+        public static void viewKundvagn()
+        {
+            Console.WriteLine("Press (2) to view cart.");
+            if(Console.ReadLine() == "2")
+            {
+                for (int i = 0; i < kundvagn.Count; i++)
+                {
+                    Console.WriteLine(kundvagn[i].Name + " , " + kundvagn[i].Price + "kr");
+                }
+            }
+        }
+    }
+}
